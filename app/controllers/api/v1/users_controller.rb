457f65c4d25b4affa6c8927996996e3ca69c2193
mvn-hieunguyen2-dn <hieu.nguyen2@monstar-lab.com>
2021-05-06 16:11:24 +0700

    up

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,28 +7,17 @@ class Api::V1::UsersController < ApplicationController
 
 	def create
 		user = User.new(user_params)
-<<<<<<< HEAD
-		user.save!
-		render json: {message: "Success !"}
-	rescue StandardError => e
-		render json: e, status: 404
-=======
  		if user.save!
 			user.send_activation_mail
 		end
 		render json: {message: "Please checking your email and active your account confirm !", status: 200}
 	rescue StandardError => e
 		render json: {message: e.message, status: 400}
->>>>>>> 5a8fbcfdae558364cfa89d2718d08fb0d031a91f
 	end
 
 	private
 
 		def user_params
-<<<<<<< HEAD
-			params.require(:user).permit :name, :email, :password, :password_confirmation
-=======
 			params.require(:user).permit(:name, :email, :password, :password_confirmation)
->>>>>>> 5a8fbcfdae558364cfa89d2718d08fb0d031a91f
 		end
 end
